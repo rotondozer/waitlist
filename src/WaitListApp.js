fff33730486eed68389f77c600fed5b3defe9554
rotondozer <nickrotondo724@gmail.com>
2017-10-02 12:17:26 -0400

    Repeat Link/placeholder component process waitlist
    Routes waitlist link to view. Currently contains static content. Adds
    temporary 'Home' link to remove all views.

--- src/WaitListApp.js
+++ src/WaitListApp.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom'
 
 import Tables from './Tables'
+import Waitlist from './Waitlist'
 
 class WaitListApp extends Component {
   render() {
@@ -16,11 +17,13 @@ class WaitListApp extends Component {
         <div>
           <h1>Your Restaurant Name</h1>
           <nav>
+            <Link to='/'>Home</Link>{'    '}
             <Link to='/tables'>Tables</Link>{'    '}
+            <Link to='/waitlist'>WaitList</Link>{'    '}
           </nav>
 
-
           <Route path='/tables' component={Tables} />
+          <Route path='/waitlist' component={Waitlist} />
 
         </div>
       </Router>
---
+++ src/Waitlist.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Waitlist extends Component {
+  render () {
+    return (
+      <div>
+        {/*
+           */}
+        <h3>Waitlist tab</h3>
+        <h3>Waitlist tab</h3>
+        <h3>Waitlist tab</h3>
+        <h3>Waitlist tab</h3>
+        <h3>Waitlist tab</h3>
+        <h3>Waitlist tab</h3>
+        <h3>Waitlist tab</h3>
+      </div>
+    )
+  }
+}
+
+export default Waitlist
