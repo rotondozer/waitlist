64be8c3cc98f70a84a5c1cda559884e917856fed
rotondozer <nickrotondo724@gmail.com>
2017-10-04 09:55:55 -0400

    Enhances sign in form
    Clears values after submit, removes debugger, replaces 'this' with a
    saved value of this in a var called 'self'

--- src/Login.js
+++ src/Login.js
@@ -15,7 +15,6 @@ class Login extends Component {
     const apiBaseUrl = 'http://localhost:4741'
     const self = this
 
-    debugger
     axios({
       url: apiBaseUrl + '/sign-in',
       method: 'POST',
@@ -24,13 +23,17 @@ class Login extends Component {
       },
       data: {
         'credentials': {
-          'email': this.state.email,
-          'password': this.state.password
+          'email': self.state.email,
+          'password': self.state.password
         }
       }
     })
       .then((response) => console.log(response))
       .catch((error) => console.log(error))
+    this.setState({
+      email: '',
+      password: ''
+    })
   }
 
   render () {
@@ -39,8 +42,8 @@ class Login extends Component {
         <h2>Sign In</h2>
         <form onSubmit={(event) => this.handleSubmit(event)}>
           {/* Storing username and password values in state variables which change on each keystroke in onClick (onChange?) */}
-          <input placeholder='Email' onChange={(event) => this.setState({email: event.target.value})} id='email'></input>
-          <input placeholder='Password' type='password' onChange={(event) => this.setState({password: event.target.value})} id='password'></input>
+          <input placeholder='Email' onChange={(event) => this.setState({email: event.target.value})} value={this.state.email}></input>
+          <input placeholder='Password' type='password' onChange={(event) => this.setState({password: event.target.value})} value={this.state.password}></input>
 
           <button type='submit'>Sign In</button>
         </form>
