93d4642cd89dd767c12926b441b985126a4080b1
rotondozer <nickrotondo724@gmail.com>
2017-10-04 22:44:39 -0400

    Edit works but is not user friendly
    Still need to actually redirect

--- src/EditParty.js
+++ src/EditParty.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class EditParty extends Component {
   constructor (props) {
@@ -10,12 +11,58 @@ class EditParty extends Component {
       timeIn: '',
       notes: ''
     }
+    this.editParty = this.editParty.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  render() {
-    debugger
+  editParty (event) {
+    event.preventDefault()
+    axios({
+      url: 'http://localhost:4741/parties/' + this.props.id,
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json'
+      },
+      data: {
+        party: {
+          name: this.state.name,
+          size: this.state.size,
+          checked_in: this.state.timeIn,
+          est_wait: this.state.estWait,
+          notes: this.state.notes,
+          // TODO: pass user_id down from container component
+          user_id: 1
+        }
+      }
+    })
+      .then(() => this.getAllParties())
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error))
+      {/*PLACE USER MESSAGE HERE*/}
+  }
+
+  handleChange (event) {
+    let attribute = event.target.name
+    let value = event.target.value
+    this.setState({
+      [attribute]: value
+    })
+  }
+
+  render () {
     return (
-      <h2>hey you have made it here</h2>
+      <div>
+        <form onSubmit={this.editParty}>
+          <h1>Edit this Party</h1>
+          <input name='name' placeholder='name' onChange={this.handleChange} value={this.state.name}></input>
+          <input name='size' placeholder='party size' onChange={this.handleChange} value={this.state.size}></input>
+          <input name='estWait' placeholder='estimated wait' onChange={this.handleChange} value={this.state.estWait}></input>
+          <input name='timeIn' placeholder='time checked in' onChange={this.handleChange} value={this.state.timeIn}></input>
+          <input name='notes' placeholder='notes' onChange={this.handleChange} value={this.state.notes}></input>
+
+          <button type='submit'>Edit Party</button>
+        </form>
+      </div>
     )
   }
 }
--- src/Party.js
+++ src/Party.js
@@ -7,14 +7,15 @@ class Party extends Component {
   constructor (props) {
     super (props)
     this.state = {
-      editParty: false
+      editParty: false,
+      editPartyId: ''
     }
   }
 
   render () {
     if (this.state.editParty) {
       return <Route push to='/edit_parties' render={() => (
-        <EditParty id={'testing id pass'}/>
+        <EditParty id={this.state.editPartyId}/>
       )}/>
 
     }
@@ -26,7 +27,7 @@ class Party extends Component {
         <p>Estimated Wait: {this.props.estWait}</p>
         <p>Time Checked In: {this.props.checkedIn}</p>
         <p>Notes: {this.props.notes}</p>
-        <input onClick={(event) => this.setState({editParty:true})} type='button' value='edit' id={this.props.id}/>
+        <input onClick={(event) => this.setState({editParty:true, editPartyId:event.target.id})} type='button' value='edit' id={this.props.id}/>
         <input onClick={(event) => this.props.onDeleteProp(event)} type='button' value='delete' id={this.props.id}/>
       </div>
     )
