c4abe862737d96deb7185ce51005ca88db8a07c3
rotondozer <nickrotondo724@gmail.com>
2017-10-16 14:39:26 -0400

    Slims down MatchingTables component to just
    Render and return. Not slim enough for functional component because
    I need to do mapping inside the render, outside the return.

--- src/MatchingTables.js
+++ src/MatchingTables.js
@@ -11,43 +11,12 @@ import axios from 'axios'
 import Table from './Table.js'
 
 class MatchingTables extends Component {
-  constructor (props) {
-    super (props)
-    this.state = {
-      // TODO get rid of state altogether and render based off props
-      // Use a FUNCTIONAL COMPONENT
-      matchingTablesArray: this.props.matchingTablesArray,
-      test: 'test'
-    }
-  }
-
-  // getTablesMatchingPartySize (event) {
-  //   const self = this
-  //   event.preventDefault()
-  //   axios({
-  //     url: 'http://localhost:4741/tables/' + this.props.size + '/match',
-  //     method: 'GET',
-  //     headers: {
-  //       'content-type': 'application/json',
-  //       'Authorization': 'Token token=' + this.props.token
-  //     }
-  //   })
-  //     .then((response) => {
-  //       console.log(response.data.tables)
-  //       this.props.updateMatchingTableState(response.data.tables)
-  //     })
-  //     .catch((error) => console.log(error))
-  // }
-
-
   render () {
-
     const matchingTables = this.props.matchingTablesArray.map((table, index) => <Table
       id={table.id}
       max_seat={table.max_seat}
       min_seat={table.min_seat}
       key={index}
-
     />)
 
     return (
@@ -68,7 +37,6 @@ class MatchingTables extends Component {
           </TableUI.Body>
         </TableUI>
       </Container>
-
     )
   }
 }
