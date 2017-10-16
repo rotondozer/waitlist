7fc73f990aeda4add5feeef65fc8ea43c124c58c
rotondozer <nickrotondo724@gmail.com>
2017-10-16 13:58:53 -0400

    Adds table for matching party

--- src/MatchingTables.js
+++ src/MatchingTables.js
@@ -3,14 +3,19 @@ import { Redirect } from 'react-router'
 import { Route, Link } from 'react-router-dom'
 import {
   Table as TableUI,
-  Button
+  Button,
+  Container
 } from 'semantic-ui-react'
 import axios from 'axios'
+
+import Table from './Table.js'
 
 class MatchingTables extends Component {
   constructor (props) {
     super (props)
     this.state = {
+      // TODO get rid of state altogether and render based off props
+      // Use a FUNCTIONAL COMPONENT
       matchingTablesArray: this.props.matchingTablesArray,
       test: 'test'
     }
@@ -36,12 +41,34 @@ class MatchingTables extends Component {
 
 
   render () {
-    debugger
+
+    const matchingTables = this.props.matchingTablesArray.map((table, index) => <Table
+      id={table.id}
+      max_seat={table.max_seat}
+      min_seat={table.min_seat}
+      key={index}
+
+    />)
+
     return (
-      <div>
-        <h1>{this.props.matchingTablesArray[0].id}</h1>
-        <h1>Test State {this.state.test}</h1>
-      </div>
+      <Container>
+        <TableUI celled>
+          <TableUI.Header>
+            <TableUI.Row>
+              <TableUI.HeaderCell>Number</TableUI.HeaderCell>
+              <TableUI.HeaderCell>Max Guests</TableUI.HeaderCell>
+              <TableUI.HeaderCell>Min Guests</TableUI.HeaderCell>
+              <TableUI.HeaderCell>Edit</TableUI.HeaderCell>
+              <TableUI.HeaderCell>Delete</TableUI.HeaderCell>
+            </TableUI.Row>
+          </TableUI.Header>
+
+          <TableUI.Body>
+            {matchingTables}
+          </TableUI.Body>
+        </TableUI>
+      </Container>
+
     )
   }
 }
