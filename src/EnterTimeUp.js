ea6ae8927562ea608ed399d2dde9ab878976e36e
rotondozer <nickrotondo724@gmail.com>
2017-10-17 16:39:32 -0400

    EnterTimeUp successfully PATCHES
    Need to add redirect after successful request, or at least clear the
    input field.

--- src/EnterTimeUp.js
+++ src/EnterTimeUp.js
@@ -12,8 +12,15 @@ class EnterTimeUp extends Component {
     this.state = {
       activity_id: this.props.activity_id,
       table_id: this.props.table_id,
-      time_sat: this.props.time_sat
+      time_sat: this.props.time_sat,
+      time_up: ''
     }
+  }
+
+  handleInputChange (event) {
+    this.setState({
+      time_up: event.target.value
+    })
   }
 
   submitTimeUp (event) {
@@ -28,26 +35,32 @@ class EnterTimeUp extends Component {
       data: {
         tables_activity: {
           table_id: this.state.table_id,
-          min_seat: this.state.min_seat,
-          // TODO: pass user_id down from container component
+          time_sat: this.state.time_sat,
+          // time_up is coming from the input field updating state
+          time_up: this.state.time_up,
+          // What about party_id?
           user_id: this.props.user_id
         }
       }
     })
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error))
   }
 
   render () {
     return (
       <TableUI.Row>
         <TableUI.Cell>{this.props.table_id}</TableUI.Cell>
-        <TableUI.Cell>max</TableUI.Cell>
-        <TableUI.Cell>min</TableUI.Cell>
-        <TableUI.Cell>edit</TableUI.Cell>
+        <TableUI.Cell>{'  '}</TableUI.Cell>
+        <TableUI.Cell>{'  '}</TableUI.Cell>
+        <TableUI.Cell>
+          <Button basic color='yellow' onClick={(event) => this.submitTimeUp(event)}>Submit</Button>
+        </TableUI.Cell>
         <TableUI.Cell>delete</TableUI.Cell>
         <TableUI.Cell>{this.props.time_sat}</TableUI.Cell>
         <TableUI.Cell>
           {/* TODO capture onChange within state, send that with submit form */}
-          <input placeholder='enter time up'></input>
+          <input placeholder='enter time up' onChange={(event) => this.handleInputChange(event)} value={this.state.time_up_input}></input>
         </TableUI.Cell>
 
       </TableUI.Row>
